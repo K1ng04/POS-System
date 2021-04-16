0d1a975b5722e8ecedcc837724b9cd92e50da2be
Shainil Shah <shahshainil94@gmail.com>
2021-04-16 18:20:14 -0500

    Add files via upload
    Revision to original code.

--- POSystem.py
+++ POSystem.py
@@ -1,46 +1,51 @@
 
-
-class Inventory:
+class Sales():
     def __init__(self):
-        self._inventory = {}
-
-    def get_inventory(self):
-        return self._inventory
-
-    def set_inventory(self, product_name, price, qty, product_id):
-        self._inventory.update({product_name: [price, qty, product_id]})
-
-    def check_inventory(self, product_name):
-        for key in self._inventory.keys():
-            if product_name == key:
-                return f'Product Available, we have {self._inventory.get(product_name)[1]} left'
-
-
-class Register:
-    def __init__(self, cash=1000):
-        self._cash = cash
-
-    def drawer(self):
-        return self._cash
-
-
-class Checkout(Inventory, Register):
+        self.Sales = {}
+    def get_sale(self):
+        return self.Sales
+    def set_Sales(self, name, price ,quantity):
+        self.Sales = {name: {"Price": price, "Quantity": quantity}}
+class Inventory(Sales):
     def __init__(self):
-        self._cart = {}
-        self._total = 0
         super().__init__()
+        self.Inventory = {}
+    def set_Inventory(self, name, price, quant):
+        self.Inventory[name] = {"Price": price, "Quantity": quant}
+    def get_Inventory(self):
+        return self.Inventory
+    def remove_Inventory(self, name, quantity):
+        if self.Inventory.get(name) != None:
+            self.Sales[name] = {"Product": name, "Quantity": quantity} 
+            if self.Inventory[name]["Quantity"] == 1:
+                print(f'There is only {self.Inventory[name]["Quantity"]} left')
+                ans = input("Would you like to Continue (Y/N): ")
+                if ans.upper() == 'Y' or ans.upper == "YES":
+                    self.Inventory[name]["Quantity"] -= quantity 
+                    price = self.Inventory[name]["Price"]
+                    Sales.set_Sales(self, name, price, quantity)
+                    self.Inventory.pop(f'{name}', None)
+            elif self.Inventory[name]["Quantity"] > 1: 
+                price = self.Inventory[name]["Price"]
+                Sales().set_Sales(name, price, quantity)
+                self.Inventory[name]["Quantity"] -= quantity
+            else:
+                 print(f'{name} does not exist in the inventory')
+        else:
+            print(f'{name} does not exist in the inventory')
 
-    def add_to_cart(self, product_name, qty):
-        for key in self._inventory.keys():
-            if product_name == key:
-                self._cart.update({product_name: qty})
-                self._total += self._cart.get(product_name)[1]
 
-    def get_total(self):
-        return self._total
 
-    def check_out(self, amount_paid):
-        if self._total < amount_paid:
-            change = amount_paid - self._total
-            print(f'Thank You, your change is {change}')
 
+
+# class Checkout(Inventory): 
+
+y = Inventory()
+y.set_Inventory('Rolex', 15000,2)
+y.set_Inventory('Patek Philippe', 100000, 1)
+y.set_Inventory('Hublot', 50000, 5)
+y.set_Inventory('Richard Mille', 100000, 5)
+print(y.get_Inventory())
+y.remove_Inventory('Richard Mille', 4)
+print(y.get_Inventory())
+print(y.get_sale())
